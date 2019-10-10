c60b158920103ad992f7f500c98a2205f1ad1d8a
DimkoB <45150184+DimkoB@users.noreply.github.com>
2019-10-10 15:06:53 +0300

    Module1_1

---
+++ Module1_1/Module1_1/Program.cs
@@ -0,0 +1,24 @@
+﻿ using System;
+
+namespace Module1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+
+            b = a + b;
+            a = b - a;
+            b = b - a;
+
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.ReadKey();
+        }
+    }
+}
