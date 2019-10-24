4292b529747c8955e5ede4a634ced6262a209dfe
DimkoB <45150184+DimkoB@users.noreply.github.com>
2019-10-24 22:51:07 +0300

    Module3_Task2

---
+++ Module3_Task2/Module3_Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Module3_Task2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the number N: ");
+            int N = int.Parse(Console.ReadLine());
+            int i = 1;
+            int k = 1;
+
+            while (true)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                    k++;
+                }
+                if (k > N)
+                {
+                    break;
+                }
+                i++;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
