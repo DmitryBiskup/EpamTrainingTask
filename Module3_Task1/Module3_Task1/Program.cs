d6239fb9680f232cabfd9eac192c057872705675
DimkoB <45150184+DimkoB@users.noreply.github.com>
2019-10-24 22:49:40 +0300

    Module3_Task1

---
+++ Module3_Task1/Module3_Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Module3_Task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the number a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the number b:");
+            int b = int.Parse(Console.ReadLine());
+            int result = 0;
+
+            for (int i = 1; i <= b; i++)
+            {
+                result += a;
+            }
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+    }
+}
