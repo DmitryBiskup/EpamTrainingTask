64316b41ded97c477cc49bd54da3023abe035614
DimkoB <45150184+DimkoB@users.noreply.github.com>
2019-10-30 12:36:31 +0300

    Module3_Task7

---
+++ Module3_Task7/Module3_Task7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Module3_Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("array[{0}]=", i);
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 1; i < array.Length; i++)
+                if (array[i] > array[i - 1])
+                    Console.Write(" " + array[i]);
+            Console.ReadKey();
+        }
+    }
+}
