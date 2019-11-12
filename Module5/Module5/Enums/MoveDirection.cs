d0825dc4cca73f29b17e29c2e7e46c686113a721
DimkoB <45150184+DimkoB@users.noreply.github.com>
2019-11-13 02:16:16 +0300

    Module5

---
+++ Module5/Module5/Enums/MoveDirection.cs
@@ -0,0 +1,10 @@
+﻿namespace EpamGame.Enums
+{
+    public enum MoveDirection
+    {
+        Top = 38,
+        Right = 39,
+        Bottom = 40,
+        Left = 37
+    }
+}
---
+++ Module5/Module5/Models/BoardElement.cs
@@ -0,0 +1,20 @@
+﻿using EpamGame.Structs;
+
+namespace EpamGame.Models
+{
+    public abstract class BoardElement
+    {
+        public Coordinates ElementCoordinates { get; protected set; }
+        protected abstract string BoardSymbol { get; }
+
+        protected BoardElement(Coordinates coordinates)
+        {
+            ElementCoordinates = coordinates;
+        }
+
+        public string RenderElement()
+        {
+            return BoardSymbol;
+        }
+    }
+}
